6156c7daa71394c5d43250f8c044e75e76e42b9e
Mahdi <mahdiparivash@protonmail.com>
2025-10-20 17:34:00 +0330

    Implement client-side token validation in DashboardLayout
    - Added a useEffect hook to check for a token in localStorage and redirect to the home page if not found, enhancing security for the dashboard.
    - Removed commented-out Breadcrumb component to clean up the layout code.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { AppSidebar } from "@/components/dashboard/app-sidebar";
 import {
   SidebarInset,
@@ -5,39 +7,28 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 import type React from "react";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("/");
+    }
+  }, [router]);
   return (
     <SidebarProvider>
       <AppSidebar side="right" />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center text-[#000e1f] bg-[#F6F5EE] gap-2 border-b px-4">
-          {/* <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink className="text-[#000e1f]" href="#">
-                  ثبت حواله
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage className="text-[#000e1f]">
-                  داشبورد
-                </BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb> */}
           <SidebarTrigger className="-mr-1 ml-auto rotate-180" />
         </header>
         <div
