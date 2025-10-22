90958403d18c3f26e14ef963373732879f28eb02
Mahdi <86263611+Mhdph@users.noreply.github.com>
2025-10-22 21:05:34 +0330

    Replace logo in DashboardHeader and update image dimensions; add new logo asset.

--- components/dashboard/header.tsx
+++ components/dashboard/header.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils";
 import { Sidebar } from "./sidebar";
 import { SidebarTrigger } from "../ui/sidebar";
 import Image from "next/image";
-import logo from "@/assets/images/zavran-dark.png";
+import logo from "@/assets/images/logo-no-name.png";
 
 export function DashboardHeader() {
   const [currentTime, setCurrentTime] = useState(new Date());
@@ -49,28 +49,33 @@ export function DashboardHeader() {
         <Image
           src={logo}
           alt="logo"
-          width={80}
-          height={80}
+          width={60}
+          height={60}
           className="mr-5 md:hidden"
         />
 
         {/* Right - Menu */}
-        <div className="flex items-center gap-2 col-span-1">
-          <span className="text-gray-300 text-xs font-normal">مدیر آفلاین</span>
-          <div className="bg-gray-300 rounded-full p-[2px]">
-            <div className="w-1 h-1 bg-gray-600 rounded-full"></div>
-          </div>
-        </div>
       </div>
 
       {/* Date/Time Bar */}
       <div className="bg-gray-200 border-t border-gray-300 px-4 py-2">
         <div className="flex items-center justify-between">
-          <div className="text-gray-700 text-sm font-medium">
-            {formatDate(currentTime)}
+          <div className="flex items-center ">
+            <div className="text-gray-700 text-sm font-medium">
+              {formatDate(currentTime)}
+            </div>
+            <span className="text-gray-700 text-sm font-medium">-</span>
+            <div className="text-gray-700 text-sm font-medium">
+              {formatTime(currentTime)}
+            </div>
           </div>
-          <div className="text-gray-700 text-sm font-medium">
-            {formatTime(currentTime)}
+          <div className="flex items-center gap-2 ">
+            <span className="text-slate-800 text-sm font-medium">
+              مدیر آفلاین
+            </span>
+            <div className="bg-gray-300 rounded-full p-[2px]">
+              <div className="w-1 h-1 bg-gray-600 rounded-full"></div>
+            </div>
           </div>
         </div>
       </div>
